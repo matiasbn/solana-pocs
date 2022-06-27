86ab1f1954ee913e65ad2f1d782c22a7810bcea2
Matías Barrios <matias.barriosn@gmail.com>
2022-06-27 19:01:46 -0400

    funding user account with signer

--- pda-account-ownership/poc/programs/poc/src/lib.rs
+++ pda-account-ownership/poc/programs/poc/src/lib.rs
@@ -1,14 +1,80 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::pubkey::PUBKEY_BYTES};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod poc {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_proxy_escrow(ctx: Context<CreateProxyEscrow>) -> Result<()> {
+        let proxy_escrow = &mut ctx.accounts.proxy_escrow;
+        msg!(
+            "proxy_escrow_owner: {}",
+            proxy_escrow.to_account_info().owner
+        );
+        proxy_escrow.escrow_owner = ctx.accounts.payer.key();
+        proxy_escrow.rewards_last_claimed_at = 0;
+        Ok(())
+    }
+
+    pub fn vulnerable_instruction(ctx: Context<VulnerableInstruction>) -> Result<()> {
+        let proxy_escrow = &mut ctx.accounts.proxy_escrow;
+        msg!(
+            "proxy_escrow_owner: {}",
+            proxy_escrow.to_account_info().owner
+        );
+        proxy_escrow.escrow_owner = ctx.accounts.payer.key();
+        proxy_escrow.rewards_last_claimed_at = 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct VulnerableInstruction<'info> {
+    /// [Escrow].
+    pub proxy_escrow: Account<'info, ProxyEscrow>,
+
+    /// Payer of the initialization.
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// System program.
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateProxyEscrow<'info> {
+    /// [Escrow].
+    #[account(
+        init,
+        seeds = [
+            b"ProxyEscrow".as_ref(),
+            payer.key().to_bytes().as_ref()
+        ],
+        bump,
+        payer = payer,
+        space = 8 + ProxyEscrow::LEN
+    )]
+    pub proxy_escrow: Account<'info, ProxyEscrow>,
+
+    /// Payer of the initialization.
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// System program.
+    pub system_program: Program<'info, System>,
+}
+
+/// records rewards state on a user.
+#[account]
+#[derive(Copy, Debug, Default)]
+pub struct ProxyEscrow {
+    /// The key of the account that is authorized to withdraw from the [Treasury].
+    pub escrow_owner: Pubkey,
+    /// When the [proxy_escrow::escrow_owner] last claimed rewards from [Treasury].
+    pub rewards_last_claimed_at: i64,
+}
+
+impl ProxyEscrow {
+    /// Number of bytes in a [ProxyEscrow].
+    pub const LEN: usize = PUBKEY_BYTES + 8;
+}
--- pda-account-ownership/poc/tests/poc.ts
+++ pda-account-ownership/poc/tests/poc.ts
@@ -1,17 +1,63 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Poc } from '../target/types/poc';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import {
+  createWithSeedSync,
+  findProgramAddressSync,
+} from "@project-serum/anchor/dist/cjs/utils/pubkey";
+import { Poc } from "../target/types/poc";
+import { expect } from "chai";
 
-describe('poc', () => {
-
+describe("poc", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Poc as Program<Poc>;
+  const { connection, wallet: signer } = program.provider;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+  it("Is initialized!", async () => {
+    const user = await anchor.web3.Keypair.generate();
+    const initialTransfer = 1 * anchor.web3.LAMPORTS_PER_SOL;
+    const recentBlockhash = await connection.getLatestBlockhash();
+    const transaction = new anchor.web3.Transaction({
+      ...recentBlockhash,
+      feePayer: signer.publicKey,
+    }).add(
+      anchor.web3.SystemProgram.transfer({
+        fromPubkey: signer.publicKey,
+        toPubkey: user.publicKey,
+        lamports: initialTransfer,
+      })
+    );
+    const signedTx = await signer.signTransaction(transaction);
+    await anchor.web3.sendAndConfirmRawTransaction(
+      connection,
+      signedTx.serialize()
+    );
+    const userBalance = await connection.getBalance(user.publicKey);
+    expect(userBalance).to.be.eql(initialTransfer);
+
+    const [proxyEscrowAccountId, bump] = await findProgramAddressSync(
+      [Buffer.from("ProxyEscrow"), signer.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const tx = await program.rpc.createProxyEscrow({
+      accounts: {
+        proxyEscrow: proxyEscrowAccountId,
+        payer: signer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+    });
+
     console.log("Your transaction signature", tx);
+
+    const { escrowOwner, rewardsLastClaimedAt } =
+      await program.account.proxyEscrow.fetch(proxyEscrowAccountId);
+
+    console.log(escrowOwner.toBase58());
+
+    expect(escrowOwner.toBase58()).to.be.eql(signer.publicKey.toBase58());
+
+    // const createAccountInstruction = await program.account.proxyEscrow.createInstruction()
   });
 });
